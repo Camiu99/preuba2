216e146c4f07b31f390b0f1f4aee9b4104713c5f
cmcalvopina <cmcalvopina@espe.edu.ec>
2021-11-04 18:36:47 -0500

    ahora shi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatbotComponent } from './chatbot/chatbot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, ChatbotComponent, ChatbotComponent
   ],
   imports: [
     BrowserModule,
