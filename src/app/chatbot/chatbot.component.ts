4edf6d1b69f09d1c150b109cdbdd119094ebfa8d
cmcalvopina <cmcalvopina@espe.edu.ec>
2021-11-05 11:11:38 -0500

    version 2.0

--- src/app/chatbot/chatbot.component.ts
+++ src/app/chatbot/chatbot.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-chatbot',
-  templateUrl: './chatbot.component.html',
-  styleUrls: ['./chatbot.component.css']
+  templateUrl: './chatbot.component.html'
 })
 export class ChatbotComponent implements OnInit {
 
